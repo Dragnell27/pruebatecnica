195f9eddc59d084555cb8f862f91f0ffc65826df
Dragnell27 <jaduvase27@gmail.com>
2024-08-26 10:46:23 -0500

    .

--- backend/database/migrations/2024_08_23_235042_create_params_table.php
+++ backend/database/migrations/2024_08_23_235042_create_params_table.php
@@ -14,6 +14,7 @@ class CreateParamsTable extends Migration
      */
     public function up()
     {
+        //Creo la tabla para almacenar parametros.
         Schema::create('params', function (Blueprint $table) {
             $table->id();
             $table->string('name');
